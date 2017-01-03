7bbe0e544c46cd89667f3799f567a6f07c5c9c7c
renzosunico <r.sunico@klab.com>
2017-01-03 17:19:28 +0800

    Added regex.

--- apps/jarvis/utilities.py
+++ apps/jarvis/utilities.py
@@ -5,6 +5,6 @@ import string
 def clean_string(text):
     """Cleans string."""
     text = re.sub(r' +', ' ', text)  # remove extra spaces
-    text = re.sub(r'[^A-Za-z0-9 ]', '', text)
+    text = re.sub(r'[^A-Za-z0-9 \'.\",]', '', text)
     text = text.strip()
     return text
